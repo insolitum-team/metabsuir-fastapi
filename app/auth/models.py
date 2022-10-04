7c9328bb0cdd2351f1fda492e0b5a3446f754b54
ImmortalDante <maklakovfdr@gmail.com>
2022-10-04 17:46:01 +0300

    auth services first step

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,1 +1,14 @@
 # Database models
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+	__tablename__ = "metabsuir_users"
+
+	id = Column(Integer, primary_key=True, index=True, unique=True)
+	username = Column(String(20))
+	email = Column(String(50))
+	password = Column(String(50))
--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ load_dotenv()
 DATABASE_URL =\
     f'postgresql://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
 
-print(DATABASE_URL)
+JWT_SECRET = os.getenv("JWT_SECRET")
