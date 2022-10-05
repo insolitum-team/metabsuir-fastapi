babcbc49ec8604c19b833cf7d2ca7dc122210470
esquiresword <71277778+esquiresword@users.noreply.github.com>
2022-10-05 13:32:29 +0300

    models, routes and schemas for themes and messages added

--- app/forum/router.py
+++ app/forum/router.py
@@ -1,12 +1,13 @@
 # Core of each module with all the endpoints
 
-from app.auth import constants as auth_constants
-from app.auth.constants import ErrorCode as PostsErrorCode
+# from app.auth import constants as auth_constants
+# from app.auth.constants import ErrorCode as PostsErrorCode
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from app.database import get_session
-from app.forum.service import get_section_list, create_section_service
-from app.forum.schemas import SectionCreate, SectionList
+
+from app.forum.schemas import SectionCreate, SectionList, ThemeCreate, ThemeList, MessageCreate, MessageList
+from app.forum.service import ForumService
+from app.auth.schemas import UserModel
+from app.auth.service import get_user
 
 router = APIRouter(
     prefix="/forum",
@@ -14,12 +15,34 @@ router = APIRouter(
 )
 
 
-# todo Create response model
-@router.get("/", response_model=list[SectionList])
-def section_list(db: Session = Depends(get_session)):
-    return get_section_list(db)
+# --------- Sections routes --------- #
+@router.get("/sections", response_model=list[SectionList])
+def section_list(service: ForumService = Depends()):
+    return service.get_section_list()
 
 
-@router.post("/")
-def create_section(item: SectionCreate, db: Session = Depends(get_session)):
-    return create_section_service(db, item)
+@router.post("/sections")
+def create_section(section_data: SectionCreate, service: ForumService = Depends()):
+    return service.create_section_service(section_data)
+
+
+# --------- Themes routes --------- #
+@router.get("/themes", response_model=list[ThemeList])
+def theme_list(service: ForumService = Depends()):
+    return service.get_theme_list()
+
+
+@router.post("/themes")
+def create_theme(theme_data: ThemeCreate, service: ForumService = Depends()):
+    return service.create_theme_service(theme_data)
+
+
+# --------- Messages routes --------- #
+@router.get("/messages", response_model=list[MessageList])
+def message_list(service: ForumService = Depends()):
+    return service.get_message_list()
+
+
+@router.post("/messages")
+def create_message(message_data: MessageCreate, user: UserModel = Depends(get_user), service: ForumService = Depends()):
+    return service.create_message_service(message_data, user.id)
--- app/forum/schemas.py
+++ app/forum/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
+# --------- Sections --------- #
 class SectionBase(BaseModel):
     title: str
     date: datetime
@@ -11,9 +12,44 @@ class SectionBase(BaseModel):
         orm_mode = True
 
 
-class SectionList(BaseModel):
+class SectionList(SectionBase):
     id: int
 
 
 class SectionCreate(SectionBase):
     pass
+
+
+# --------- Themes --------- #
+class ThemeBase(BaseModel):
+    title: str
+    date: datetime
+    user_id = int
+
+    class Config:
+        orm_mode = True
+
+
+class ThemeList(ThemeBase):
+    id: int
+
+
+class ThemeCreate(ThemeBase):
+    pass
+
+
+# --------- Messages --------- #
+class MessageBase(BaseModel):
+    theme = str
+    date = datetime
+
+    class Config:
+        orm_mode = True
+
+
+class MessageList(MessageBase):
+    id: int
+
+
+class MessageCreate(MessageBase):
+    content: str
