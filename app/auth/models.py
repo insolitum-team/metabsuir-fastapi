149fbbf80bf9a38ade4f314cfea8d27245e916fe
esquiresword <71277778+esquiresword@users.noreply.github.com>
2022-10-05 19:35:31 +0300

    unique keys added

--- app/auth/models.py
+++ app/auth/models.py
@@ -8,6 +8,6 @@ class User(Base):
 	__tablename__ = "metabsuir_users"
 
 	id = Column(Integer, primary_key=True, index=True, unique=True)
-	username = Column(String(20))
-	email = Column(String(50))
+	username = Column(String(20), unique=True)
+	email = Column(String(50), unique=True)
 	password = Column(String(255))
