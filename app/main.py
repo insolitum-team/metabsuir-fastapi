139a3e21a47afa88a7f927bd164c5b953f7f9868
ImmortalDante <maklakovfdr@gmail.com>
2022-11-03 07:08:08 +0300

    email setup v2

--- app/main.py
+++ app/main.py
@@ -5,20 +5,25 @@ from fastapi import FastAPI
 from app.auth.router import router as auth_router
 from app.forum.router import router as forum_router
 from app.profile.router import router as profile_router
+from app.subjects.router import router as subject_router
 # from app.database import SessionLocal
 
 tags_metadata = [
     {
         "name": "forum",
-        "description": "MetaBSUIR forum"
+        "description": "MetaBSUIR forum",
     },
     {
         "name": "authentication",
-        "description": "MetaBSUIR authentication"
+        "description": "MetaBSUIR authentication",
     },
     {
         "name": "profile",
-        "description": "Members profile"
+        "description": "Members profile",
+    },
+    {
+        "name": "subjects",
+        "description": "Subjects profile",
     },
 ]
 
@@ -33,6 +38,7 @@ def get_application() -> FastAPI:
     application.include_router(auth_router)
     application.include_router(forum_router)
     application.include_router(profile_router)
+    application.include_router(subject_router)
     return application
 
 
