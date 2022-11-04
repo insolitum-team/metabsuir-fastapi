fd7e39d0db6e12a2f5b224b96f32bda3cfe3f705
ImmortalDante <maklakovfdr@gmail.com>
2022-11-04 15:26:18 +0300

    4 empty routes

--- app/subjects/router.py
+++ app/subjects/router.py
@@ -5,3 +5,23 @@ router = APIRouter(
     prefix="/subjects",
     tags=["subjects"],
 )
+
+
+@router.get("/get-subject")
+def get_subject_route():
+    pass
+
+
+@router.get("/get-subject-additional")
+def get_subject_additional_route():
+    pass
+
+
+@router.post("/add-subject")
+def add_subject_route():
+    pass
+
+
+@router.post("/add-subject-additional")
+def add_subject_additional():
+    pass
